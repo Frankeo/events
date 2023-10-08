0b01faa9ed1b410b296d67950255e0ce22a1b7e2
Francisco Leonel Moreno Eraso <f.moreno@aligntech.com>
2023-10-08 23:33:42 +0200

    Testing deplyo to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://frankeo.github.io',
-    base: "/events"
+  output: "server",
+  adapter: netlify()
 });
